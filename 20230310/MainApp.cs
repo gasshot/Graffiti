d93abb6a2fb8e7f91a883cb637683b1d3af6f4ec
인오 강 <kanginoh@gmail.com>
2023-03-11 11:55:40 +0900

    초기 커밋.

---
+++ 20230310/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _20230310
+{
+    class MainApp
+    {
+        
+        static void Main(string[] args)
+        {
+
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            int a = int.Parse(input1);
+            int b = int.Parse(input2); ;
+
+            Console.WriteLine($"a > 0 && b < 0 : {a > 0 && b < 0}");
+            Console.WriteLine($"\na > 0 || b < 0 : {a > 0 || b < 0}");
+
+
+        }
+    }
+}
