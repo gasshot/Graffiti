9fe19e5fc2f2ac57a16ef3f53f03977f623a3410
인오 강 <kanginoh@gmail.com>
2023-03-30 09:06:49 +0900

    202303300906

--- Subject/MainApp.cs
+++ Subject/MainApp.cs
@@ -1,100 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Subject
 {
 
     class MainApp
     {
-        static int[] CardNumber(int a)
+        public static void CardMaker(int a)
         {
-            int min = 1;
-            int max = 13;
-            int count = a;
-            int[] cNArray = new int[count];
-            Random cardnumber = new Random();
-
-            for (int loop = 0; loop < count; loop++)
+            int cardnumber = 13;
+            int[] card = new int[cardnumber];
+            for (int i = 0; i < cardnumber; i++)
             {
-                // 랜덤 값 생성
-                int randNumber = cardnumber.Next(min, max);
-
-                // 랜덤 값이 배열에 존재하면 loop를 1 감소
-                if (cNArray.Contains(randNumber))
-                {
-                    loop--;
-                }
-                // 랜덤 값이 배열에 없으면 배열에 추가
-                else
-                {
-                    cNArray[loop] = randNumber;
-                }
+                card[i] = i + 1;
+                //Console.WriteLine(card[i]);
             }
-            return cNArray;
-        }
-
-        static int[] FigureNumber(int a)
-        {
-            int min = 1;
-            int max = 4;
-            int count = a;
-            int[] fArray = new int[count];
-            Random figurenum = new Random();
-            for (int loop = 0; loop < count; loop++)
-            {
-                // 랜덤 값 생성
-                int randNumber = figurenum.Next(min, max);
-                fArray[loop] = randNumber;             
-            }
-            return fArray;
-        }
-
-        public string Merge(int a, int b)
-        {
-            string Majesty = "";
-            List<string> figure = new List<string> { "Diamond", "Spade", "Clover", "Heart" };
-
-
-            if (a > 10 || a == 1)
-            {
-                switch (a)
-                {
-                    case 1:
-                        Majesty = "A";
-                        break;
-                    case 11:
-                        Majesty = "J";
-                        break;
-                    case 12:
-                        Majesty = "Q";
-                        break;
-                    case 13:
-                        Majesty = "K";
-                        break;
-                }
-            }
-            else
-            {
-                Majesty = Convert.ToString(a);
-            }
-
-            string x = Majesty + " " + figure[b];
-            return x;
+            List<int[]> deck = new List<int[]> { };
+            int[] cards = new int[13];
+            Console.WriteLine(deck);
         }
 
         static void Main(string[] args)
         {
-            int z = 5;
-            int[] k = CardNumber(z);
-            int[] f = FigureNumber(z);
-            
-            for (int i = 0; i < z; i++)
-            {
-                MainApp merge = new MainApp();
-                Console.WriteLine(merge.Merge(k[i],f[i]));
-            }
+            int x = 0;
+            int round = 7;
+            int[] playercard = new int[round];
+            int[] computercard = new int[round];
 
+            CardMaker(x);
 
         }
     }
---
+++ Subject02/MainApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Subject02
+{
+    class MainApp
+    {
+        public static string[] CardMaster()
+        {
+            int[] cardn = new int[52];        //1~13/14~26/27~39/40~52
+            string figure = "";
+            int number = 0;
+            string[] cardf = new string[52];
+            for (int i = 0; i < 52; i++)
+            {
+                if (i < 13)
+                {
+                    figure = "Clover";
+                    number = i + 1;
+                }
+                if (13 <= i && i < 26)
+                {
+                    figure = "Heart";
+                    number = i + 1 - 13;
+                }
+                else if (26 <= i && i < 39)
+                {
+                    figure = "Spade";
+                    number = i + 1 - 26;
+                }
+                else if (39 <= i && i < 52)
+                {
+                    figure = "Diamond";
+                    number = i + 1 - 39;
+                }
+                cardn[i] = number;
+                cardf[i] = figure;
+                //Console.WriteLine(card[i]+$"-{i+1}");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Random cardnumber = new Random();
+            int[] cNArray = new int[5];
+
+            for (int loop = 0; loop < 5; loop++)
+            {
+                // 랜덤 값 생성
+                int randNumber = cardnumber.Next(0, 51);
+
+                // 랜덤 값이 배열에 존재하면 loop를 1 감소
+                if (cNArray.Contains(randNumber))
+                {
+                    loop--;
+                    continue;
+                }
+                // 랜덤 값이 배열에 없으면 배열에 추가
+                else
+                {
+                    cNArray[loop] = randNumber;
+                }
+
+                Console.WriteLine(cardn[randNumber] + " " + cardf[randNumber]);
+            }
+        }
+    }
+}
+
+
